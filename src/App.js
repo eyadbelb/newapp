555d687ffea9514b2b86dcc96dea8797f733df7e
eyadbelb <eyadbelb@gmail.com>
2021-06-01 11:24:17 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="theme">
+      <h1 className="headTitle"> Drums Shop</h1>
+      <p>
+        {" "}
+        As for the lack of the drums in our classes i decided to open an drums
+        shop especially for Laila{" "}
+      </p>
+      <p> If you are a Coded student there is a decount for you</p>
+
+      <img src="https://scontent.famm7-1.fna.fbcdn.net/v/t1.6435-9/62033236_2731231410237787_6560041788445294592_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=6e5ad9&_nc_ohc=M0EmOnOWvtoAX9i0L0-&_nc_ht=scontent.famm7-1.fna&oh=a8021a34197e51d9fc203e124b1573e8&oe=60DC8A15" />
     </div>
   );
 }
