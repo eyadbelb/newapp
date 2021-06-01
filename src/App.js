bae568b72ce44ff374d970e2ff2b6cba107fccd1
eyadbelb <eyadbelb@gmail.com>
2021-06-01 11:30:14 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         As for the lack of the drums in our classes i decided to open a drums
         shop especially for Laila{" "}
       </p>
-      <p> If you are a Coded student there is a decount for you</p>
+      <p> If you are a Coded student there is a discount for you</p>
 
       <img src="https://scontent.famm7-1.fna.fbcdn.net/v/t1.6435-9/62033236_2731231410237787_6560041788445294592_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=6e5ad9&_nc_ohc=M0EmOnOWvtoAX9i0L0-&_nc_ht=scontent.famm7-1.fna&oh=a8021a34197e51d9fc203e124b1573e8&oe=60DC8A15" />
     </div>
